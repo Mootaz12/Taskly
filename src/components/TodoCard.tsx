62310a6565fb3bb6a1fe145ee5513c1a82f8661b
Mootaz Belleh Weslati <mootazbellehweslati@gmail.com>
2024-07-23 02:08:09 +0100

    fixed design

--- src/components/TodoCard.tsx
+++ src/components/TodoCard.tsx
@@ -6,9 +6,9 @@ import UpdateTodoBtn from "./UpdateTodoBtn";
 const TodoCard: React.FC<TodoCardPropsType> = ({ todo }) => {
   return (
     <div className="bg-white shadow-md rounded-lg p-4 mb-4 hover:bg-gray-100 flex gap-5 justify-between ">
-      <div className="flex flex-col items-center justify-center gap-3">
+      <div className="flex flex-col items-center justify-center gap-3 text-center ">
         <p className="text-xl font-semibold">{todo.title}</p>
-        <p className="text-gray-700">{todo.description}</p>
+        <p className="text-gray-700 max-w-[500px]">{todo.description}</p>
       </div>
       <div className="flex flex-col items-center justify-center gap-3">
         <UpdateTodoBtn todo={todo} />
